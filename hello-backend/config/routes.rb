693751fbb9c99fd684892e559fd459c319598039
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-03 15:11:58 +0300

    Add routes

--- hello-backend/config/routes.rb
+++ hello-backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: %i[index show] do
+        collection { get :random }
+      end
+    end
+  end
 end
