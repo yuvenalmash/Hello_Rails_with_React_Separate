bc9443f92b4907e97cddefe292a30ac951f22af1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-03 22:29:34 +0300

    Remove the frontend folder

--- hello-frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import greetingReducer from './greetingSlice';
-
-export default configureStore({
-  reducer: {
-    greeting: greetingReducer,
-  },
-});
