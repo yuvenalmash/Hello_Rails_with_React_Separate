7fdf992c153fcba332ea69eb97bd1cf85e205f39
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-03 15:10:34 +0300

    Add greetings to populate the messages table

--- hello-backend/db/seeds.rb
+++ hello-backend/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+messages =
+  Message.create(
+    [
+      { greeting: "[English]: Hello, World!" },
+      { greeting: "[French]: Bonjour, le monde!" },
+      { greeting: "[Italian]: Ciao, mondo!" },
+      { greeting: "[Japanese]: Kon'nichiwa sekai!" },
+      { greeting: "[Swahili]: Hujambo, dunia!" }
+    ]
+  )
