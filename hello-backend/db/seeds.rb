a8953257b930f592efb7e981cb13afd74885ab42
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-03 19:51:09 +0300

    fix linter error

--- hello-backend/db/seeds.rb
+++ hello-backend/db/seeds.rb
@@ -6,13 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #
-messages =
-  Message.create(
-    [
-      { greeting: '[English]: Hello, World!' },
-      { greeting: '[French]: Bonjour, le monde!' },
-      { greeting: '[Italian]: Ciao, mondo!' },
-      { greeting: "[Japanese]: Kon'nichiwa sekai!" },
-      { greeting: '[Swahili]: Hujambo, dunia!' }
-    ]
-  )
+Message.create(
+  [
+    { greeting: "[English]: Hello, World!" },
+    { greeting: "[French]: Bonjour, le monde!" },
+    { greeting: "[Italian]: Ciao, mondo!" },
+    { greeting: "[Japanese]: Kon'nichiwa sekai!" },
+    { greeting: "[Swahili]: Hujambo, dunia!" }
+  ]
+)
